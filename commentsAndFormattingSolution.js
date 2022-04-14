775cd15846de14e09b42636aaaba7eed15cbb114
Tzachi Elrom <tzachie@tailormed.co>
2022-04-14 12:29:48 +0300

    updated hashIt

--- commentsAndFormattingSolution.js
+++ commentsAndFormattingSolution.js
@@ -22,23 +22,19 @@ doStuff();
 // doSoMuchStuff();
 
 //4
-const hashIt = (data) => {
-  // The hash
-  let hash = 0;
 
-  // Length of string
+function hashIt(data) {
+  let hash = 0;
   const length = data.length;
 
-  // Loop through every character in data
   for (let i = 0; i < length; i++) {
-    // Get character code.
     const char = data.charCodeAt(i);
-    // Make the hash
     hash = (hash << 5) - hash + char;
+
     // Convert to 32-bit integer
     hash &= hash;
   }
-};
+}
 
 //formatting:
 //1
