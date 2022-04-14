6313b16846a9156ff8807b0cef62736464222afb
Tzachi Elrom <tzachie@tailormed.co>
2022-04-14 12:28:40 +0300

    updated hashIt

--- commentsAndFormattingTask.js
+++ commentsAndFormattingTask.js
@@ -26,13 +26,18 @@ const actions = function () {
 doStuff();
 
 function hashIt(data) {
+  // The hash
   let hash = 0;
+
+  // Length of string
   const length = data.length;
 
+  // Loop through every character in data
   for (let i = 0; i < length; i++) {
+    // Get character code.
     const char = data.charCodeAt(i);
+    // Make the hash
     hash = (hash << 5) - hash + char;
-
     // Convert to 32-bit integer
     hash &= hash;
   }
